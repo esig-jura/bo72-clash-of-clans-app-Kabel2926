664cb2fb7e76f8564529924a516315a9b3053ec0
theo.oberli <theo.oberli@divcom.ch>
2024-05-08 11:47:50 +0200

    création des composants

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,34 @@
 <script setup>
 // Cheat Sheet: https://steve-fallet.notion.site/Vue-3-script-setup-Cheat-Sheet-b12192ceae244ecda65f771579ca02bc
-import {ref} from 'vue'
+import {onMounted, ref} from "vue";
+import PageTopBarre from "@/components/PageTopBarre.vue";
+import PageHeader from "@/components/PageHeader.vue";
+import PageFooter from "@/components/PageFooter.vue";
+
+// Quand le composant est monté, on va chercher les données
+onMounted(() => {
+  fetch('https://cocapi.divtec.me/troupes')
+      .then((res) => res.json())
+      .then((data) => {
+        troupes.value = data
+      })
+})
+
 
 // Tableau des troupes
-const troupes = ref([
-  {
-    "id": 2,
-    "nom": "Archer",
-    "niveau": 3,
-    "formation": 25,
-    "vitesse": 24,
-    "cout": 200,
-    "description": "Les archers sont des tireurs d'élite qui attaquent à distance. Ils sont rapides et bon marché à former, mais ils sont faibles en mêlée et doivent être protégés.",
-    "couleur": "#EE5487",
-    "image": "https://cocapi.divtec.me/img/archer.png"
-  },
-  {
-    "id": 1,
-    "nom": "Barbare",
-    "niveau": 4,
-    "formation": 20,
-    "vitesse": 16,
-    "cout": 150,
-    "description": "Ce guerrier intrépide compte sur ses muscles saillants et sa fière moustache pour semer le chaos dans les villages ennemis. Faites charger une horde de barbares et profitez du spectacle !",
-    "couleur": "#ebb428",
-    "image": "https://cocapi.divtec.me/img/barbare.png"
-  },
-  {
-    "id": 3,
-    "nom": "Géant",
-    "niveau": 5,
-    "formation": 12,
-    "vitesse": 12,
-    "cout": 2250,
-    "description": "Les géants sont de grands guerriers qui attaquent les défenses ennemies en premier. Ils sont lents mais résistants.",
-    "couleur": "#F6901A",
-    "image": "https://cocapi.divtec.me/img/geant.png"
-  }
-])
+const troupes = ref([])
+
 </script>
 
 <template>
-  <h1>Clash Of Clans</h1>
-  <ul>
-    <li v-for="troupe in troupes" :key="troupe">
-      {{ troupe.nom }}
-    </li>
-  </ul>
-  <aside class="solde-or">
-  <div>
-    <img src="/img/piece-or-note.jpg" alt="Solde Or">
-    20 000 pièces d'or
-  </div>
-  <div>
-    <img src="/img/troupes-icon.png" alt="Troupes">
-    0 troupes formées
-  </div>
-  </aside>
-  <header>
-    <h1>
-      <img src="/img/clash-of-clans-logo.webp" alt="Logo Clash of Clans">
-    </h1>
-    <p class="description">
-      Construire un village,
-      former un clan et participer à des guerres de clans épiques !
-    </p>
-  </header>
+  <page-top-barre or=""/>
+  <page-header/>
   <main>
     <ul class="cartes">
       <li  v-for="troupe in troupes" :key="troupe.id">
         <article>
-          <header :style="'background: linear-gradient(60deg,' + troupe.couleur + ' 0%, ' + troupe.couleur + ' 100%);'">
-            <img :src="troupe.image" :alt="troupe.nom">
+          <header :style="'background: linear-gradient(60deg, #3B3B3B  0%, ' + troupe.couleur + ' 100%);'">
+          <img :src="troupe.image" :alt="troupe.nom">
           </header>
           <div class="level" :style="'color: ' + troupe.couleur + ' ;'">
             Niveau {{ troupe.niveau }}
@@ -99,8 +56,6 @@ const troupes = ref([
       </li>
     </ul>
   </main>
-  <footer>
-    &copy; 2023 - Supercell.com
-  </footer>
+  <page-footer/>
 </template>
 
---
+++ src/components/PageTopBarre.vue
@@ -0,0 +1,44 @@
+<script setup>
+defineProps({
+  or: {
+    type: Number,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <aside>
+    <div>
+      <img src="/img/piece-or-note.jpg" alt="Solde Or">
+      {{ or.toLocaleString('fr-CH') }}
+      pièces d'or
+    </div>
+    <div>
+      <img src="/img/troupes-icon.png" alt="Troupes">
+      0 troupes formées
+    </div>
+  </aside>
+</template>
+
+<style scoped>
+aside {
+  position: fixed;
+  width: 100%;
+  background: white;
+  color: #3B3B3B;
+  font-size: 1.25rem;
+  padding: .5rem;
+  z-index: 9999;
+  box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 1em;
+}
+
+img {
+  max-height: 3rem;
+  vertical-align: middle;
+}
+</style>
